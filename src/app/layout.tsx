ced80f29373d21133b0c7bffc245405dba12f487
Azvya Erstevan <azvyae@gmail.com>
2024-07-03 12:53:32 +0700

    Only use one opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,10 +31,7 @@ export const metadata: Metadata = {
     "Welcome to Optimisticoder, it's your code, be optimist! Find new insights, tips, or other case studies related to programming and tech here.",
   keywords: 'blog, portfolio, programming, code, azvya, erstevan',
   openGraph: {
-    images: [
-      '/img/opengraph/optimisticoder-bright.jpg',
-      '/img/opengraph/optimisticoder-dark.jpg',
-    ],
+    images: ['/img/opengraph/optimisticoder-dark.jpg'],
   },
 };
 
