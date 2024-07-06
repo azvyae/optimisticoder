0066a0e70d8764c055679a0f1e251e60a32210ee
Azvya Erstevan <azvyae@gmail.com>
2024-07-06 17:45:08 +0700

    Added navigation configurations

--- src/config/common/index.ts
+++ src/config/common/index.ts
@@ -1,2 +1,3 @@
-export * from '@/config/common/main';
 export * from '@/config/common/apps';
+export * from '@/config/common/main';
+export * from '@/config/common/navigation';
---
+++ src/config/common/navigation.ts
@@ -0,0 +1,22 @@
+import type { LinkType } from '@/types/common';
+
+const links: LinkType[] = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Stories',
+    href: '/stories',
+  },
+  {
+    name: 'Portfolio',
+    href: '/portfolio',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+  },
+];
+
+export { links };
