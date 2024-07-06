d9ef124605f98d9782a096df1c38dff4bfd02b93
Azvya Erstevan <azvyae@gmail.com>
2024-07-06 17:45:23 +0700

    Finalized navbar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-
 async function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen h-[1500px] flex-col items-center justify-between p-24">
       <p className="text-3xl">Coming soon.</p>
-      <Link href={'/test'}>Go Test</Link>
     </main>
   );
 }
