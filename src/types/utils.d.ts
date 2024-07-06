dd1d8584889b98ce3024757073a8d114262bb260
Azvya Erstevan <azvyae@gmail.com>
2024-07-06 17:44:32 +0700

    Added standard types

---
+++ src/types/utils.d.ts
@@ -0,0 +1,32 @@
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> &
+      Partial<Record<Exclude<Keys, K>, undefined>>;
+  }[Keys];
+
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = [],
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+type BasicColors = 'success' | 'info' | 'warning' | 'danger';
+
+type MergeObject<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
