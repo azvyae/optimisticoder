7842aae85836a2a79165a89946182e2948d1a66b
Azvya Erstevan <azvyae@gmail.com>
2024-08-28 21:38:31 +0700

    Fixed sitemap data

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,22 +9,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://optimisticoder.com/portfolio',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://optimisticoder.com/contact',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.6,
-    },
-    {
       url: 'https://optimisticoder.com/stories',
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.5,
+      priority: 0.8,
     },
     ...(generatedSitemap as MetadataRoute.Sitemap),
     {
